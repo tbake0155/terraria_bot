d656aa3c524f10a2588dd2aedc5ffae5e0da7f34
Timothy Baker <tbake0155@gmail.com>
2018-08-08 08:53:08 -0400

    updates

--- mouseEvent.py
+++ mouseEvent.py
@@ -1,11 +1,9 @@
 import win32api
 import win32con
-import time
+from time import sleep
 
 def leftClick():
-    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -1, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.1)
+    sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-    time.sleep(0.1)
 
--- terraria_bot.py
+++ terraria_bot.py
@@ -1,3 +1,7 @@
+import argparse
+import cv2
+import numpy as np
+import os
 import threading
 import win32api
 import mouseEvent
@@ -5,12 +9,31 @@ import keyEvent
 import pyautogui
 from time import sleep
 
+screen_width, screen_height = pyautogui.size()
+
+search_width_left = screen_width - (3 * (screen_width / 4))
+search_height_top = screen_height - (2 * (screen_height / 3))
+
 print_lock = threading.Lock()
 
 def thread_safe_print(*args):
     with print_lock:
         for arg in args:
             print arg
+
+screenshot_lock = threading.Lock()
+
+screenshot = pyautogui.screenshot
+
+def Screenshot():
+    with screenshot_lock:
+        global screenshot
+        return screenshot
+
+def Screenshot(new_screenshot):
+    with screenshot_lock:
+        global screenshot
+        screenshot = new_screenshot
 
 explorer = False
 trigger = False
@@ -137,25 +160,44 @@ class ExitWatcher(threading.Thread):
     def stopped(self):
         return self._stop_event.is_set()
 
+class EagleEye(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._stop_event = threading.Event() 
+
+    def run(self):
+        while True:
+            new_screenshot = pyautogui.screenshot()
+            Screenshot(new_screenshot)
+            sleep(0.03)
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
 def main():
-    explorerWatcher = ExplorerWatcher()
+    #explorerWatcher = ExplorerWatcher()
     triggerWatcher = TriggerWatcher()
     exitWatcher = ExitWatcher()
+    #eagleEye = EagleEye()
 
-    explorerWatcher.start()
+    #explorerWatcher.start()
     triggerWatcher.start()
     exitWatcher.start()
-
+    #eagleEye.start()
+    
     while not quitter:
         if trigger:
             mouseEvent.leftClick()
-        else:
-            sleep(0.01)
-        if explorer:
-            keyEvent.pressAndHold('a','s')
+        #if explorer:
+        #    keyEvent.pressAndHold('a','s')
+        sleep(0.01)
 
-    explorerWatcher.stop()
+    #explorerWatcher.stop()
     triggerWatcher.stop()
-    exitWatcher.stop()    
+    exitWatcher.stop()
+    #eagleEye.stop()
 
 main()
